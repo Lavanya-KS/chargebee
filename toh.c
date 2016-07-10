f1b2b8b11dc6e05d0b5e8afcbf17048b0606a27c
Lavanya-KS <lavsreedhar312@gmail.com>
2016-07-10 12:01:31 +0530

    Create toh.c

---
+++ toh.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+void toh(int num_of_disk, char fromrod, char torod, char auxrod)
+{
+    if (num_of_disk == 1)
+    {
+        printf("\n Move disk 1 from rod %c to rod %c", fromrod, torod);
+        return;
+    }
+    toh(num_of_disk-1, fromrod, auxrod, torod);
+    printf("\n Move disk %d from rod %c to rod %c", num_of_disk, fromrod, torod);
+    toh(num_of_disk-1, auxrod, torod, fromrod);
+}
+
+int main()
+{
+    int disk = 3; // Number of disks
+    toh(disk, 'A', 'C', 'B');  // A, B and C are names of rods
+    return 0;
+}
